b9c3c9015ca23f08bffac54daf67627db0366408
Hiro Tanaka <HTanaka@lcs.on.ca>
2024-05-30 11:46:24 -0400

    Initial Commit

---
+++ ClassReview/ClassReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassReviewApp.swift
+//  ClassReview
+//
+//  Created by 田中宏貴 on 2024-05-30.
+//
+
+import SwiftUI
+
+@main
+struct ClassReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
